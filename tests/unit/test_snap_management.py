cbf2227b51a2a0117df7d84c7fa8c3f2ed3b93b1
Andrew Scribner <ca.scribner+1@gmail.com>
2024-06-24 10:52:44 -0400

    refactor test_snap_management.py based on review feedback

--- tests/unit/test_snap_management.py
+++ tests/unit/test_snap_management.py
@@ -34,66 +34,66 @@ minimal_snap_spec = {
 }
 
 
-class TestSnapInstall:
+@patch("snap_management.snap_lib.SnapCache")
+def test_install_snap(mocked_cache):
+    name = "some-charm"
+    revision = "123"
+    classic = False
+    snap = mocked_cache()[name]
+    _install_snap(name=name, revision=revision, classic=classic)
 
-    @patch("snap_management.snap_lib.SnapCache")
-    def test_install_snap(self, mocked_cache):
-        name = "some-charm"
-        revision = "123"
-        classic = False
-        snap = mocked_cache()[name]
-        _install_snap(name=name, revision=revision, classic=classic)
-
-        snap.ensure.assert_called_once_with(
-            state=snap_management.snap_lib.SnapState.Present,
-            revision=str(revision),
-            classic=classic,
-        )
-        snap.hold.assert_called_once()
-
-    @patch("snap_management._grafana_agent_snap_spec", snap_spec)
-    @patch("snap_management.get_system_arch")
-    @patch("snap_management._install_snap")
-    @pytest.mark.parametrize(
-        "classic, arch, expected",
-        [
-            (False, "amd64", snap_spec["strict"]["amd64"]),
-            (False, "arm64", snap_spec["strict"]["arm64"]),
-            (True, "some-arch", snap_spec["classic"]["some-arch"]),
-        ],
+    snap.ensure.assert_called_once_with(
+        state=snap_management.snap_lib.SnapState.Present,
+        revision=str(revision),
+        classic=classic,
     )
-    def test_install_ga_snap(
-        self, mocked_install_snap, mocked_get_system_arch, classic, arch, expected
-    ):
-        # Arrange
-        mocked_get_system_arch.return_value = arch
+    snap.hold.assert_called_once()
 
-        # Act
+
+@patch("snap_management._grafana_agent_snap_spec", snap_spec)
+@patch("snap_management.get_system_arch")
+@patch("snap_management._install_snap")
+@pytest.mark.parametrize(
+    "classic, arch, expected",
+    [
+        (False, "amd64", snap_spec["strict"]["amd64"]),
+        (False, "arm64", snap_spec["strict"]["arm64"]),
+        (True, "some-arch", snap_spec["classic"]["some-arch"]),
+    ],
+)
+def test_install_ga_snap(
+    mocked_install_snap, mocked_get_system_arch, classic, arch, expected
+):
+    # Arrange
+    mocked_get_system_arch.return_value = arch
+
+    # Act
+    install_ga_snap(classic=classic)
+
+    # Assert
+    mocked_get_system_arch.assert_called_once()
+    mocked_install_snap.assert_called_once_with(
+        name=expected["name"], revision=expected["revision"], classic=classic
+    )
+
+
+@patch("snap_management._grafana_agent_snap_spec", minimal_snap_spec)
+@patch("snap_management.get_system_arch")
+@patch("snap_management._install_snap")
+@pytest.mark.parametrize(
+    "classic, arch, context_raised",
+    [
+        (True, "amd64", pytest.raises(SnapSpecError)),
+        (False, "arm64", pytest.raises(SnapSpecError)),
+        (False, "amd64", does_not_raise()),
+    ],
+)
+def test_install_ga_snap_validation_errors(
+    mocked_install_snap, mocked_get_system_arch, classic, arch, context_raised
+):
+    # Arrange
+    mocked_get_system_arch.return_value = arch
+
+    # Act
+    with context_raised:
         install_ga_snap(classic=classic)
-
-        # Assert
-        mocked_get_system_arch.assert_called_once()
-        mocked_install_snap.assert_called_once_with(
-            name=expected["name"], revision=expected["revision"], classic=classic
-        )
-
-    @patch("snap_management._grafana_agent_snap_spec", minimal_snap_spec)
-    @patch("snap_management.get_system_arch")
-    @patch("snap_management._install_snap")
-    @pytest.mark.parametrize(
-        "classic, arch, context_raised",
-        [
-            (True, "amd64", pytest.raises(SnapSpecError)),
-            (False, "arm64", pytest.raises(SnapSpecError)),
-            (False, "amd64", does_not_raise()),
-        ],
-    )
-    def test_install_ga_snap_validation_errors(
-        self, mocked_install_snap, mocked_get_system_arch, classic, arch, context_raised
-    ):
-        # Arrange
-        mocked_get_system_arch.return_value = arch
-
-        # Act
-        with context_raised:
-            install_ga_snap(classic=classic)
