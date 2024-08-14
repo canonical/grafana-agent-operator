f1c478e5ec1979628094b277028e9338bb44bc82
Andrew Scribner <ca.scribner+1@gmail.com>
2024-08-14 15:21:30 -0400

    fix: formatting in test_cloud_integration.py

--- tests/unit/test_cloud_integration.py
+++ tests/unit/test_cloud_integration.py
@@ -1,13 +1,12 @@
 import tempfile
-from pathlib import Path
 import unittest
+from pathlib import Path
 from unittest.mock import patch
 
 import yaml
-from ops import BlockedStatus, ActiveStatus
-from ops.testing import Harness
-
 from charm import GrafanaAgentMachineCharm as GrafanaAgentCharm
+from ops import ActiveStatus, BlockedStatus
+from ops.testing import Harness
 
 
 class TestUpdateStatus(unittest.TestCase):
@@ -38,7 +37,9 @@ class TestUpdateStatus(unittest.TestCase):
         harness.set_leader(True)
         harness.begin_with_initial_hooks()
 
-        self._subtest_prometheus_remote_write_config_with_grafana_cloud_integrator_on_leader(harness)
+        self._subtest_prometheus_remote_write_config_with_grafana_cloud_integrator_on_leader(
+            harness
+        )
 
     def test_prometheus_remote_write_config_with_grafana_cloud_integrator_on_non_leader(self):
         """Asserts that the prometheus remote write config is written correctly when the charm is not a leader."""
@@ -48,9 +49,13 @@ class TestUpdateStatus(unittest.TestCase):
         harness.set_leader(False)
         harness.begin_with_initial_hooks()
 
-        self._subtest_prometheus_remote_write_config_with_grafana_cloud_integrator_on_leader(harness)
+        self._subtest_prometheus_remote_write_config_with_grafana_cloud_integrator_on_leader(
+            harness
+        )
 
-    def _subtest_prometheus_remote_write_config_with_grafana_cloud_integrator_on_leader(self, harness):
+    def _subtest_prometheus_remote_write_config_with_grafana_cloud_integrator_on_leader(
+        self, harness
+    ):
         """Helper for all shared code between prometheus_remote_write_config tests."""
         # WHEN an incoming relation is added
         rel_id = harness.add_relation("juju-info", "grafana-agent")
@@ -61,7 +66,11 @@ class TestUpdateStatus(unittest.TestCase):
 
         # AND WHEN all the necessary outgoing relations are added
         # for outgoing in ["send-remote-write", "logging-consumer"]:
-        harness.add_relation("grafana-cloud-config", "grafana-cloud-integrator", app_data={"prometheus_url": "http://some.domain.name:9090/api/v1/write"})
+        harness.add_relation(
+            "grafana-cloud-config",
+            "grafana-cloud-integrator",
+            app_data={"prometheus_url": "http://some.domain.name:9090/api/v1/write"},
+        )
 
         # THEN the charm goes into active status
         assert isinstance(harness.charm.unit.status, ActiveStatus)
@@ -69,4 +78,7 @@ class TestUpdateStatus(unittest.TestCase):
         # THEN with the expected prometheus endpoint settings are written to the config file
         config = yaml.safe_load(Path(self.config_path_mock).read_text())
         assert len(config["integrations"]["prometheus_remote_write"]) == 1
-        assert config["integrations"]["prometheus_remote_write"][0]["url"] == "http://some.domain.name:9090/api/v1/write"
+        assert (
+            config["integrations"]["prometheus_remote_write"][0]["url"]
+            == "http://some.domain.name:9090/api/v1/write"
+        )
