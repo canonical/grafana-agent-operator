398f0d6a0700da49ae83b8723e650c5f4c21e18b
Dylan Stephano-Shachter <dylan.stephano-shachter@canonical.com>
2024-08-23 14:21:55 +0000

    linting

--- tests/integration/test_classic_config_flag.py
+++ tests/integration/test_classic_config_flag.py
@@ -1,8 +1,6 @@
 # Copyright 2023 Canonical Ltd.
 # See LICENSE file for licensing details.
 
-import asyncio
-import json
 import logging
 from types import SimpleNamespace
 from typing import List
@@ -29,9 +27,7 @@ async def ssh(ops_test, app_name: str, command: str) -> List[str]:
 @pytest.mark.abort_on_fail
 async def test_build_and_deploy(ops_test: OpsTest, grafana_agent_charm):
     # Principal
-    await ops_test.model.deploy(
-        principal.charm, application_name=principal.name, series="jammy"
-    )
+    await ops_test.model.deploy(principal.charm, application_name=principal.name, series="jammy")
 
     # Workaround: `charmcraft pack` produces two files, but `ops_test.build_charm` returns the
     # first one. Since primary is jammy, we know we need to deploy the jammy grafana agent charm,
@@ -66,9 +62,7 @@ async def test_classic_by_default(ops_test: OpsTest):
     # $ juju ssh agent/0 snap services grafana-agent
     # Service                      Startup  Current  Notes
     # grafana-agent.grafana-agent  enabled  active   -
-    out = await ssh(
-        ops_test, agent.name, "snap info --verbose grafana-agent | grep confinement"
-    )
+    out = await ssh(ops_test, agent.name, "snap info --verbose grafana-agent | grep confinement")
     assert out.split()[1] == "classic"
 
 
@@ -81,10 +75,10 @@ async def test_switch_to_strict(ops_test: OpsTest):
     # Service                      Startup  Current  Notes
     # grafana-agent.grafana-agent  enabled  active   -
     ops_test.model.applications[agent.name].set_config({"classic_snap": False})
-    await ops_test.model.wait_for_idle(apps=[principal.name, agent.name], status="active", idle_period=15)
-    out = await ssh(
-        ops_test, agent.name, "snap info --verbose grafana-agent | grep confinement"
+    await ops_test.model.wait_for_idle(
+        apps=[principal.name, agent.name], status="active", idle_period=15
     )
+    out = await ssh(ops_test, agent.name, "snap info --verbose grafana-agent | grep confinement")
     assert out.split()[1] == "strict"
 
 
@@ -97,8 +91,8 @@ async def test_switch_to_classic(ops_test: OpsTest):
     # Service                      Startup  Current  Notes
     # grafana-agent.grafana-agent  enabled  active   -
     ops_test.model.applications[agent.name].set_config({"classic_snap": True})
-    await ops_test.model.wait_for_idle(apps=[principal.name, agent.name], status="active", idle_period=15)
-    out = await ssh(
-        ops_test, agent.name, "snap info --verbose grafana-agent | grep confinement"
+    await ops_test.model.wait_for_idle(
+        apps=[principal.name, agent.name], status="active", idle_period=15
     )
+    out = await ssh(ops_test, agent.name, "snap info --verbose grafana-agent | grep confinement")
     assert out.split()[1] == "classic"
