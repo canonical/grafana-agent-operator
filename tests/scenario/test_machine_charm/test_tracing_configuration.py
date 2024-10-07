5df539739e1c10531f4a05198b7dd0a0638ca67d
Mateusz Kulewicz <mateusz.kulewicz@canonical.com>
2024-10-07 13:12:34 +0200

    Review remarks: better naming, test code

--- tests/scenario/test_machine_charm/test_tracing_configuration.py
+++ tests/scenario/test_machine_charm/test_tracing_configuration.py
@@ -1,11 +1,18 @@
 from typing import get_args
+from unittest.mock import patch
 
 import pytest
+import yaml
 from charms.grafana_agent.v0.cos_agent import ReceiverProtocol
 from charms.tempo_k8s.v2.tracing import ReceiverProtocol as TracingReceiverProtocol
-from scenario import Context, State
+from scenario import Context, Relation, State, SubordinateRelation
 
 from charm import GrafanaAgentMachineCharm
+from lib.charms.grafana_agent.v0.cos_agent import (
+    CosAgentProviderUnitData,
+    Receiver,
+)
+from lib.charms.tempo_k8s.v2.tracing import TracingProviderAppData
 
 
 def test_cos_agent_receiver_protocols_match_with_tracing():
@@ -28,3 +35,56 @@ def test_always_enable_config_variables_are_generated_for_tracing_protocols(
     with context.manager("config-changed", state) as mgr:
         charm: GrafanaAgentMachineCharm = mgr.charm
         assert protocol in charm.requested_tracing_protocols
+
+
+@pytest.mark.parametrize(
+    "sampling_config",
+    (
+        {
+            "always_enable_otlp_http": True,
+        },
+        {
+            "always_enable_otlp_http": True,
+            "tracing_sample_rate_charm": 23.0,
+            "tracing_sample_rate_workload": 13.13,
+            "tracing_sample_rate_error": 42.42,
+        },
+    ),
+)
+def test_tracing_sampling_config_is_present(
+    vroot, placeholder_cfg_path, mock_config_path, sampling_config
+):
+    # GIVEN a tracing relation over the tracing-provider endpoint and one over tracing
+    context = Context(
+        charm_type=GrafanaAgentMachineCharm,
+        charm_root=vroot,
+    )
+    tracing_provider = SubordinateRelation(
+        "cos-agent",
+        remote_unit_data=CosAgentProviderUnitData(
+            metrics_alert_rules={},
+            log_alert_rules={},
+            metrics_scrape_jobs=[],
+            log_slots=[],
+            dashboards=[],
+            subordinate=True,
+            tracing_protocols=["otlp_http", "otlp_grpc"],
+        ).dump(),
+    )
+    tracing = Relation(
+        "tracing",
+        remote_app_data=TracingProviderAppData(
+            receivers=[
+                Receiver(protocol={"name": "otlp_grpc", "type": "grpc"}, url="http:foo.com:1111")
+            ]
+        ).dump(),
+    )
+
+    state = State(leader=True, relations=[tracing, tracing_provider], config=sampling_config)
+    # WHEN we process any setup event for the relation
+    with patch("charm.GrafanaAgentMachineCharm.is_ready", True):
+        context.run("config_changed", state)
+
+    yml = yaml.safe_load(placeholder_cfg_path.read_text())
+
+    assert yml["traces"]["configs"][0]["tail_sampling"]
