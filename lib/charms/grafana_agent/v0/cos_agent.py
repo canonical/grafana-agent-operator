1f004395f89ed96290687b1e1af2ac3e6af08202
Leon <82407168+sed-i@users.noreply.github.com>
2025-01-17 09:10:24 -0500

    Add note about limit: 1 for the provider.
    Signed-off-by: Leon <82407168+sed-i@users.noreply.github.com>

--- lib/charms/grafana_agent/v0/cos_agent.py
+++ lib/charms/grafana_agent/v0/cos_agent.py
@@ -8,6 +8,7 @@ This library can be used to manage the cos_agent relation interface:
 - `COSAgentProvider`: Use in machine charms that need to have a workload's metrics
   or logs scraped, or forward rule files or dashboards to Prometheus, Loki or Grafana through
   the Grafana Agent machine charm.
+  NOTE: Be sure to add `limit: 1` in your charm for the cos-agent relation.
 
 - `COSAgentConsumer`: Used in the Grafana Agent machine charm to manage the requirer side of
   the `cos_agent` interface.
@@ -252,7 +253,7 @@ if TYPE_CHECKING:
 
 LIBID = "dc15fa84cef84ce58155fb84f6c6213a"
 LIBAPI = 0
-LIBPATCH = 15
+LIBPATCH = 16
 
 PYDEPS = ["cosl", "pydantic"]
 
