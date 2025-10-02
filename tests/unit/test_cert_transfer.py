0d5eeb59ae9e117866bd72a4c5fffea91c9b0b6b
Sina <sina.pahlavan@canonical.com>
2025-10-02 17:41:35 -0400

    chore: add test

---
+++ tests/unit/test_cert_transfer.py
@@ -0,0 +1,53 @@
+import json
+import uuid
+from unittest.mock import patch
+
+from ops.testing import Context, Model, Relation, State
+
+from charm import GrafanaAgentMachineCharm as GrafanaAgentCharm
+
+ctx = Context(GrafanaAgentCharm)
+
+def test_ca_cert_saved_to_disk():
+    delete_path = ""
+    def mock_delete(_, path):
+        nonlocal delete_path
+        delete_path = path
+
+    model_uuid = uuid.uuid4()
+    ca_cert_path = "/usr/local/share/ca-certificates"
+    fake_cert = "-----BEGIN CERTIFICATE-----123-----END CERTIFICATE-----"
+    remote_app_data = {
+        "certificates": json.dumps([
+            fake_cert
+        ]),
+        "version": "1"
+    }
+
+    # GIVEN a receive-ca-cert relation over certificates_transfer
+    rel_id = 1
+    certificate_transfer_relation = Relation(
+        "receive-ca-cert",
+        remote_app_data=remote_app_data,
+        id=rel_id,
+    )
+    state = State(
+        leader=True,
+        relations=[
+            certificate_transfer_relation
+        ],
+        model=Model(uuid=str(model_uuid))
+    )
+
+    # WHEN a relation is joined
+    cert_path = f"{ca_cert_path}/receive-ca-cert-{model_uuid}-{rel_id}-0-ca.crt"
+    with patch(
+        "charm.GrafanaAgentMachineCharm.delete_file", new=mock_delete
+    ), patch(
+        "os.listdir", return_value=[cert_path]
+    ):
+        ctx.run(ctx.on.relation_broken(certificate_transfer_relation), state)
+
+    # THEN the file must be deleted from disk
+    assert delete_path == cert_path
+
